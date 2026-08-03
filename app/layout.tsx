3af60e3b480e5949d56d838a3f7127d29d3418bb
arifnurhadi <humanajaib3317@gmail.com>
2026-08-03 19:51:26 +0700

    repair layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,12 +23,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Afacad+Flux:wght@100..1000&display=swap"
-          rel="stylesheet"
-        ></link>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Afacad+Flux:wght@100..1000&display=swap" rel="stylesheet"/>
       </head>
       <body className="min-h-screen bg-[#FDF6EC] text-zinc-900">{children}</body>
     </html>
