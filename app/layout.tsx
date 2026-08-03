c27eae0317720c6a338b27e2ea5c27b4b0610e8c
arifnurhadi <humanajaib3317@gmail.com>
2026-08-03 20:18:34 +0700

    add web logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" type="image" href="/images/mylogo.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
