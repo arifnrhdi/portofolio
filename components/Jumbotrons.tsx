12de0599754b0816b54732b12a9620e1fcdf93b3
arifnurhadi <humanajaib3317@gmail.com>
2026-08-04 19:33:20 +0700

    change margin padding

--- components/Jumbotrons.tsx
+++ components/Jumbotrons.tsx
@@ -35,7 +35,7 @@ const Jumbotrons = () => {
               <a
                 target="_blank"
                 rel="noreferrer"
-                href="https://drive.google.com/file/d/1m5y7izVhn1nTblwqBDnRAHn3jCJaKTUX/view?usp=sharing"
+                href="https://drive.google.com/drive/folders/1TYCnCDMJOlTxHkxuJX6jAVWUbRheonTa"
                 className="rounded-xl border-[3px] border-black bg-yellow-400 px-4 lg:px-8 py-2 lg:py-4 text-xs font-bold shadow-[6px_6px_0px_#000] transition hover:translate-x-1 hover:translate-y-1 hover:shadow-none lg:text-lg"
               >
                 Resume <FontAwesomeIcon className="ml-1 text-xs lg:text-sm" icon={faArrowUpRightFromSquare} />
